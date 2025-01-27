9916a724b68fdf5ce5ac22d40b204af2d6427d5d
alam0055 <alam0055@student.monash.edu>
2025-01-27 20:27:58 +0800

    a

--- app/(dashboard)/tickets/page.jsx
+++ app/(dashboard)/tickets/page.jsx
@@ -12,11 +12,11 @@ export default function Tickets() {
     <main>
       <nav>
         <div>
-          <h2>Commission Tickets</h2>
+          <h2>All Commissions</h2>
           <p><small>Check out our commission tickets by human artists!</small></p>
         </div>
         <Link href="/tickets/create" className="ml-auto">
-          <button className="btn-primary">New Ticket</button>
+          <button className="btn-primary">New Ticket (Artists)</button>
         </Link>
       </nav>
       <Suspense fallback={<Loading />}>
