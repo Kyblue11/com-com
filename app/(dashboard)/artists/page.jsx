6b764ae040ac644e867b3d3a9bf2968a198d61a6
alam0055 <alam0055@student.monash.edu>
2025-01-29 12:46:01 +0800

    bugfix

--- app/(dashboard)/artists/page.jsx
+++ app/(dashboard)/artists/page.jsx
@@ -1,5 +1,6 @@
 import { Suspense } from 'react'
-import Loading from '../../loading'
+import { Loading } from '../loading'
+import ArtistProfile from './[id]/page'
 
 export default function ArtistPage({ params }) {
   return (
