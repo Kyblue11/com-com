5e2d9ed73d6707a4d5d4784342e132e9e39030b1
alam0055 <alam0055@student.monash.edu>
2025-01-30 09:13:33 +0800

    a

--- app/(dashboard)/tickets/create/CreateForm.jsx
+++ app/(dashboard)/tickets/create/CreateForm.jsx
@@ -1,6 +1,7 @@
 import SubmitButton from "@/app/components/SubmitButton"
 import { addTicket } from "../actions"
 
+// TODO: prob dont need this file
 export default function CreateForm() {
   return (
     <form action={addTicket} className="w-1/2">
