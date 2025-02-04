3c82bf85dcf724ec339022609ed0119e6a37321d
alam0055 <alam0055@student.monash.edu>
2025-02-04 10:50:29 +0800

    feat: changed TicketDetails page with Carousel for images and improve styling

--- app/(dashboard)/tickets/[id]/page.jsx
+++ app/(dashboard)/tickets/[id]/page.jsx
@@ -3,8 +3,8 @@ import { notFound } from 'next/navigation'
 import { getTicket } from '../../services/tickets'
 import { getServerComponentClient } from '../../supabaseClient'
 
-// components
 import DeleteIcon from './DeleteIcon'
+import Carousel from '../../../components/Carousel'
 
 export const dynamicParams = true
 
@@ -28,34 +28,38 @@ export default async function TicketDetails({ params }) {
   const { data } = await supabase.auth.getSession()
 
   return (
-    <main>
-      <nav>
-        <h2>Commission Ticket Details</h2>
-        <div className="ml-auto">
-          {data.session.user.email === ticket.user_email && (
-            <DeleteIcon id={ticket.id} />
-          )}
-        </div>
-      </nav>
-      <div className="ticket-details-container">
-        <div className="ticket-details-left">
-          {Array.isArray(ticket.commission_pics.urls) && ticket.commission_pics.urls.map((url, index) => (
-            <img key={index} src={url} alt="Commission" className="commission-pic" />
-          ))}
-        </div>
-        <div className="ticket-details-right">
-          <h3>{ticket.title}</h3>
-          <small></small>
-          <p>{ticket.body}</p>
-          <p>Artist: <Link href={`/artists/${ticket.artist_id}`}>{ticket.artists.name}</Link></p>
-          <p>Additional attributes:</p>
-          <ul>
-            <li>Price: RM{ticket.price}</li>
-            <li>Attribute 2: {ticket.attribute2}</li>
-            {/*TODO: add more attributes as needed */}
-          </ul>
+    <div className="flex flex-col bg-white">
+      <div className="flex overflow-hidden flex-col items-center px-20 pt-6 pb-20 w-full bg-white max-md:px-5 max-md:pb-16 max-md:max-w-full">
+        <div className="mt-0 mb-0 w-full max-w-[1250px] max-md:mt-6 max-md:mb-2.5 max-md:max-w-full">
+          <main>
+            <nav className="flex justify-between items-center pb-4 border-b-2 border-gray-200 my-10 mx-auto w-full">
+              <h2 className="text-3xl font-bold text-stone-900">Commission Ticket Details</h2>
+              <div className="ml-auto">
+                {data.session.user.email === ticket.user_email && (
+                  <DeleteIcon id={ticket.id} />
+                )}
+              </div>
+            </nav>
+            <div className="ticket-details-container">
+              <div className="ticket-details-left">
+              {Array.isArray(ticket.commission_pics.urls) && (
+                <Carousel images={ticket.commission_pics.urls} />
+              )}
+              </div>
+              <div className="ticket-details-right">
+                <h3 className="text-3xl font-bold text-stone-900">{ticket.title}</h3>
+                <small>ㅤ</small>
+                <p className="text-lg font-medium text-stone-900 text-opacity-50">{ticket.body}</p>
+                <p className="text-lg font-medium text-stone-900">Artist: <Link href={`/artists/${ticket.artist_id}`} className="text-primary underline">{ticket.artists.name}</Link></p>
+                <p className="text-lg font-medium text-stone-900">Commission Details:</p>
+                <ul className="list-disc list-inside">
+                  <li className="text-lg font-medium text-stone-900">Price: RM{ticket.price}</li>
+                </ul>
+              </div>
+            </div>
+          </main>
         </div>
       </div>
-    </main>
+    </div>
   )
 }
