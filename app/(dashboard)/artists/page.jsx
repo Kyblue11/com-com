bf8621489d7e3a86e005e4fa805e373302d21f60
alam0055 <alam0055@student.monash.edu>
2025-02-04 15:56:31 +0800

    feat: implement Artists page with dynamic metadata and artist listing

--- app/(dashboard)/artists/page.jsx
+++ app/(dashboard)/artists/page.jsx
@@ -1,11 +1,34 @@
 import { Suspense } from 'react'
 import Loading from '../loading'
-import ArtistProfile from './[id]/page'
+import { getArtists } from '../services/artists'
+import Link from 'next/link'
 
-export default function ArtistPage({ params }) {
+export const dynamicParams = true
+
+export async function generateMetadata() {
+  return {
+    title: 'ComCom | Artists'
+  }
+}
+
+export default async function Artists() {
+  const artists = await getArtists()
+
   return (
     <Suspense fallback={<Loading />}>
-      <ArtistProfile params={params} />
+      <main>
+        <h2 className="text-3xl font-bold text-stone-900">All Artists</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
+          {artists.map(artist => (
+            <div key={artist.artist_id} className="card">
+              <Link href={`/artists/${artist.artist_id}`}>
+                <img src={artist.profile_picture} alt={artist.name} className="artist-profile-pic" />
+                <h3 className="text-xl font-bold text-stone-900">{artist.name}</h3>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </main>
     </Suspense>
   )
 }
--- app/(dashboard)/services/artists.js
+++ app/(dashboard)/services/artists.js
@@ -1,5 +1,22 @@
 import { getServerComponentClient } from '../supabaseClient'
 
+export async function getArtists() {
+  const supabase = getServerComponentClient()
+
+  const { data, error } = await supabase.from('artists')
+    .select(`
+      artist_id,
+      name,
+      profile_picture
+    `)
+
+  if (error) {
+    console.log(error.message)
+  }
+
+  return data
+}
+
 export async function getArtist(id) {
   const supabase = getServerComponentClient()
 
